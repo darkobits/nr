99dc9d0d23cb6dca845569ac223a72b1cf930bd6
Joshua <joshua+github@frontlawn.net>
2022-02-08 20:43:31 -0800

    fix: Fix log import.

--- src/lib/log.ts
+++ src/lib/log.ts
@@ -1,5 +1,5 @@
 import LogFactory from '@darkobits/log';
 
-export { default as LogPipe } from '@darkobits/log/dist/lib/log-pipe';
+export { default as LogPipe } from '@darkobits/log/dist/lib/log-pipe.js';
 
 export default LogFactory({ heading: 'nr' });
