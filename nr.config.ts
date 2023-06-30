870f6ca705b7cfd14f825251a230cfec20f1efe4
Joshua <joshua+github@frontlawn.net>
2023-06-29 22:43:55 -0700

    chore(deps): Update dependencies.

--- nr.config.ts
+++ nr.config.ts
@@ -1,22 +1,18 @@
 import { nr } from '@darkobits/ts';
-// import nr from './dist/index.js';
 
 
 export default nr(({ command, script, isCI }) => {
-  // Modern format
-  script([
+  script('test.smoke', [
     command.node('import.test.js', { cwd: './smoke-tests/esm/' }),
     command.node('dynamic-import.test.js', { cwd: './smoke-tests/cjs' }),
     command.node('require.test.js', { cwd: './smoke-tests/cjs' })
   ], {
-    name: 'test.smoke',
     group: 'Test',
     description: 'Run smoke tests.',
     timing: true
   });
 
-  if (!isCI) script('script:test.smoke', {
-    name: 'postBuild',
+  if (!isCI) script('postBuild', 'script:test.smoke', {
     group: 'Lifecycle',
     description: '[hook] If not in a CI environment, run smoke tests after building the project.'
   });
--- src/lib/ow.ts
+++
@@ -1,7 +0,0 @@
-import { interopImportDefault } from '@darkobits/interop-import-default';
-import owExport from 'ow';
-
-// TODO: This may not be needed any more.
-const ow: typeof owExport = interopImportDefault(owExport);
-
-export default ow;
