3f876c79b5f6cf9c36849d2bcdd1f27515d89ed5
Joshua <joshua+github@frontlawn.net>
2023-07-09 21:08:25 -0700

    chore(deps): Update dependencies.

--- src/bin/cli.ts
+++ src/bin/cli.ts
@@ -3,24 +3,27 @@
 import { EOL } from 'os';
 
 import * as cli from '@darkobits/saffron';
+import isCI from 'is-ci';
 
-import { printCommandInfo } from 'lib/commands';
-import loadConfig from 'lib/configuration';
+import { command, commands, printCommandInfo } from 'lib/commands';
 import log from 'lib/log';
-import { matchScript, printScriptInfo } from 'lib/scripts';
-import { printTaskInfo } from 'lib/tasks';
+import { script, scripts, matchScript, printScriptInfo } from 'lib/scripts';
+import { task, tasks, printTaskInfo } from 'lib/tasks';
 import { parseError, heroLog } from 'lib/utils';
 
 import type { CLIArguments, UserConfigurationFn } from 'etc/types';
 
+
 const chalk = log.chalk;
+
 
 cli.command<CLIArguments, UserConfigurationFn>({
   command: '* [query]',
   description: 'Run the script matched by the provided query.',
   config: {
     auto: false,
-    fileName: 'nr'
+    fileName: 'nr',
+    explicitConfigFileParam: 'config'
   },
   builder: ({ command }) => {
     command.example('$0 test', 'Runs the script that most closely matches "test".');
@@ -65,41 +68,57 @@ cli.command<CLIArguments, UserConfigurationFn>({
     command.epilogue(chalk.gray('For full usage instructions, see https://github.com/darkobits/nr'));
   },
   handler: async saffronContext => {
+    const { argv, config, configIsEmpty, configPath } = saffronContext;
+
     try {
-      // Load the user's configuration file, which should populate the commands,
-      // tasks, and/or scripts registries.
-      await loadConfig(saffronContext);
+      // If Saffron did not find a configuration file, bail.
+      if (!configPath) throw new Error([
+        'Unable to find a configuration file.',
+        'Documentation: https://darkobits.gitbook.io/nr'
+      ].join(EOL));
 
-      const { argv } = saffronContext;
+      // If Saffron found an empty configuration file, bail.
+      if (configIsEmpty) throw new Error(
+        `Configuration file at ${chalk.green(configPath)} is empty.`
+      );
 
-      // If the --commands, --tasks, or --scripts flags were used, print
-      // information about the indicated instruction type, then bail.
+      // If the configuration file did not default-export a function, bail.
+      if (typeof config !== 'function') throw new TypeError(
+        `Expected default export of file at ${chalk.green(configPath)} to be of type "function", got "${typeof config}".`
+      );
+
+      // Invoke the user's configuration function with the necessary context.
+      // This function should create at least 1 command, task, or script.
+      await config({ command, script, task, isCI });
+
+      // If the user's configuration function did not register anything, this is
+      // likely an error; bail.
+      if (commands.size === 0 && tasks.size === 0 && scripts.size === 0) throw new Error(
+        `Configuration file ${chalk.green(configPath)} did not register any commands, tasks, or scripts.`
+      );
+
+      // If the --commands, --tasks, or --scripts flags were used, print info
+      // about the indicated instruction type, then bail.
       if (argv.commands) return printCommandInfo(saffronContext);
       if (argv.tasks) return printTaskInfo(saffronContext);
       if (argv.scripts) return printScriptInfo(saffronContext);
 
-      // Otherwise, ensure that a query was provided.
-      if (!argv.query) {
-        log.error('Missing required positional argument "query".');
-        log.error(`Run ${chalk.white('nr --help')} for usage instructions.`);
-        log.error(`Run ${chalk.white('nr --scripts')} to show a list of known scripts.`);
-        return;
-      }
+      // If none of the above flags were used, then `query` becomes a required
+      // positional argument. If not present, bail.
+      if (!argv.query) throw new Error([
+        'Missing required positional argument "query".',
+        `Run ${chalk.gray('nr --help')} for usage instructions.`,
+        `Run ${chalk.gray('nr --scripts')} to show a list of known scripts.`
+      ].join(EOL));
 
-      const script = matchScript(argv.query);
-      if (script) await script.thunk();
+      const matchedScript = matchScript(argv.query);
+      if (matchedScript) await matchedScript.thunk();
     } catch (err: any) {
       const { message, stack } = parseError(err);
-
       const errLinePrefix = `${chalk.red('⏺')}`;
-
       heroLog(message.split(EOL).map(line => `${errLinePrefix} ${chalk.red.bold(line)}`).join(EOL));
-
-      // if (log.isLevelAtLeast('verbose')) {
       heroLog(stack.split(EOL).map(line => `${errLinePrefix} ${chalk.gray.dim(line)}`).join(EOL));
-      // }
-
-      process.exit(err?.exitCode ?? 1);
+      process.exit(err?.exitCode ?? err?.code ?? 1);
     }
   }
 });
