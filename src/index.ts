1fb6276c59d6f5d75812acc7f47d7ed96473308f
Joshua <joshua+github@frontlawn.net>
2024-01-08 15:13:30 -0800

    feat: `defineConfig` handles arrays.

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,39 @@
-import type { UserConfigurationExport } from 'etc/types';
+import ow from 'ow';
+
+import type {
+  UserConfigurationExport,
+  UserConfigurationFn,
+  UserConfigurationFnContext
+} from 'etc/types';
+
+
+const IS_WRAPPED_CONFIG = Symbol('WRAPPED_CONFIG');
 
 
 /**
  * Optional helper for users who prefer to have typed configuration files.
  */
-export default function defineConfig(userConfigArgument: UserConfigurationExport): UserConfigurationExport {
-  return Array.isArray(userConfigArgument)
-    ? userConfigArgument.map(userConfigurationFn => async context => userConfigurationFn(context))
-    : async context => userConfigArgument(context);
+export default function defineConfig(userConfigArgument: UserConfigurationExport | Array<UserConfigurationExport>) {
+  ow(userConfigArgument, 'first argument', ow.any(ow.function, ow.array.ofType(ow.function)));
+
+  // Don't wrap values that have already been wrapped.
+  if (Reflect.has(userConfigArgument, IS_WRAPPED_CONFIG)) return userConfigArgument;
+
+  // Wrap and return a single function.
+  if (typeof userConfigArgument === 'function') {
+    const wrappedConfigFn = async (context: UserConfigurationFnContext) => userConfigArgument(context);
+    Reflect.defineProperty(wrappedConfigFn, IS_WRAPPED_CONFIG, { value: true });
+    return wrappedConfigFn as UserConfigurationFn;
+  }
+
+  const wrappedConfigFn = async (context: UserConfigurationFnContext) => {
+    for await (const userConfigurationFn of userConfigArgument) {
+      await userConfigurationFn(context);
+    }
+  };
+
+  Reflect.defineProperty(wrappedConfigFn, IS_WRAPPED_CONFIG, { value: true });
+  return wrappedConfigFn;
 }
 
 
